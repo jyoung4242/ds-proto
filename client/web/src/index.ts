ea564940b2398c0f80ac5f791581330cd3bd8166
Justin Young <justin_dean_young@yahoo.com>
2022-07-19 14:01:54 -0400

    updates

--- client/web/src/index.ts
+++ client/web/src/index.ts
@@ -1,2 +1,24 @@
-import "./styles/globals.css";
-const container = document.getElementById("App");
+import { UI, UIView } from "peasy-ui";
+import { GameComponent, Router, SceneTransitionComponent, iComponentModel, iSceneTransitionModel } from "./components/index";
+import "./styles/index";
+
+const myApp = document.getElementById("App");
+
+const uiStringTemplate = `
+    ${GameComponent.template}
+`;
+const model = {
+  [GameComponent.componentName]: GameComponent.model,
+  [SceneTransitionComponent.componentName]: SceneTransitionComponent.model,
+};
+console.log(model);
+let myUI: UIView;
+myUI = UI.create(myApp, uiStringTemplate, model);
+
+let intervalID = setInterval(() => {
+  UI.update();
+}, 1000 / 60);
+
+setTimeout(() => model.myContainer.screenSwitch(model.myContainer, Router.Lobby), 4000);
+setTimeout(() => model.myContainer.screenSwitch(model.myContainer, Router.Character), 6000);
+setTimeout(() => model.myContainer.screenSwitch(model.myContainer, Router.Game), 8000);
