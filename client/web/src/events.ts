0091abe44362633c58dd8978e2a79b758c4efe7a
Justin Young <justin_dean_young@yahoo.com>
2022-10-21 10:01:44 -0400

    build push

--- client/web/src/events.ts
+++ client/web/src/events.ts
@@ -189,10 +189,20 @@ export let p1Coin1: GameEventSequence = { sequence: [raisep1Coin, refreshPlayerH
 export let p2Coin1: GameEventSequence = { sequence: [raisep2Coin, refreshPlayerHand] };
 export let p3Coin1: GameEventSequence = { sequence: [raisep3Coin, refreshPlayerHand] };
 export let p4Coin1: GameEventSequence = { sequence: [raisep4Coin, refreshPlayerHand] };
-export let showCardPool: GameEventSequence = { sequence: [openCardPool, shortdelay, shortdelay] };
-export let cardpurchased: GameEventSequence = { sequence: [shortdelay, buycard] };
+export let showCardPool: GameEventSequence = { sequence: [shortdelay, openCardPool, shortdelay, shortdelay] };
+export let cardpurchased: GameEventSequence = { sequence: [shortdelay, buycard, shortdelay, shortdelay] };
 export let hideCardpool: GameEventSequence = {
-  sequence: [shortdelay, shortdelay, closeCardPool, shortdelay, indexProgressBar_cardPool, shortdelay, enableMonsterDamage],
+  sequence: [
+    shortdelay,
+    shortdelay,
+    shortdelay,
+    closeCardPool,
+    shortdelay,
+    shortdelay,
+    indexProgressBar_cardPool,
+    shortdelay,
+    enableMonsterDamage,
+  ],
 };
 export let enablemonsterDamage: GameEventSequence = { sequence: [checkforAttack, highlightMonsters] };
 export let readyToEndTurn: GameEventSequence = { sequence: [indexProgressBar_mDamage, enableEndTurn] };
